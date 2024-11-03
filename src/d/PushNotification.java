1be863400c741f33641bb7de2eb15e7b82d29b36
farid-hacibabazade <faridhaci0@gmail.com>
2024-11-03 23:21:43 +0400

    added D principle

---
+++ src/d/PushNotification.java
@@ -0,0 +1,9 @@
+package d;
+
+public class PushNotification implements Notification{
+
+    @Override
+    public void send(String message) {
+        System.out.println("Sending Push Notification: " + message);
+    }
+}
---
+++ src/d/SMSNotification.java
@@ -0,0 +1,9 @@
+package d;
+
+public class SMSNotification implements Notification{
+
+    @Override
+    public void send(String message) {
+        System.out.println("Sending SMS: " + message);
+    }
+}
